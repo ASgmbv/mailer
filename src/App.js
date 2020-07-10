95a068811c987ca6d718f42713e6fc752d7f2477
adjanybekov@gmail.com <adjanybekov@gmail.com>
2020-07-11 00:51:48 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container-contact100">
+        <div
+          className="contact100-map"
+          id="google_map"
+          data-map-x="40.722047"
+          data-map-y="-73.986422"
+          data-pin="images/icons/map-marker.png"
+          data-scrollwhell="0"
+          data-draggable="1"
+        ></div>
+
+        <div className="wrap-contact100">
+          <span className="contact100-form-symbol">
+            <img src="assets/images/icons/symbol-01.png" alt="SYMBOL-MAIL" />
+          </span>
+
+          <form className="contact100-form validate-form flex-sb flex-w">
+            <span className="contact100-form-title">
+              Become smarter in just 5 minutes
+            </span>
+
+            <span className="contact100-form-subtitle">
+              Get the daily email that makes reading the news actually
+              enjoyable. Stay informed and entertained, for free.
+            </span>
+
+            <div class="input-group mb-3">
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Enter email"
+                aria-label="Enter email"
+                aria-describedby="button-addon2"
+              />
+              <div class="input-group-append">
+                <button
+                  class="btn btn-outline-secondary"
+                  type="button"
+                  id="button-addon2"
+                >
+                  Subscribe
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
